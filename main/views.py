db8c50afcc0bb38d2f0bb70c35432d2057191185
Rabia312 <go212479@gmail.com>
2021-01-20 20:29:44 +0600

    added todo form

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo, Bookhouse
 
 # Create your views here.
@@ -20,3 +20,10 @@ def homework31(request):
 def books(request):
     book_list = Bookhouse.objects.all()
     return render(request, "books.html", {"book_list": book_list})
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, second, homework31, books
+from main.views import homepage, test, second, homework31, books, add_todo
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,6 +27,7 @@ urlpatterns = [
     path("test2/", second),
     path("homework31/", homework31, name="homework31"),
     path("books/", books, name="books"),
+    path("add_todo/", add_todo, name="add-todo"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
