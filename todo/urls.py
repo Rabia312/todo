59379aa4dba55e99cb3b1bf42a516459c1f3c247
Rabia312 <go212479@gmail.com>
2021-01-20 21:11:21 +0600

    added form to the books.html from HW33

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, second, homework31, books, add_todo
+from main.views import homepage, test, second, homework31, books, add_todo, add_book
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -28,6 +28,7 @@ urlpatterns = [
     path("homework31/", homework31, name="homework31"),
     path("books/", books, name="books"),
     path("add_todo/", add_todo, name="add-todo"),
+    path("add_book/", add_book, name="add-book"),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
