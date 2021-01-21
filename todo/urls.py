aabdd19d3a1482ccd695f6dabe480530f3e52c61
Rabia312 <go212479@gmail.com>
2021-01-21 10:29:42 +0600

    added todo mark and unmark functions

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import homepage, test, second, homework31, books, add_todo, add_book, delete_todo
+from main.views import homepage, test, second, homework31, books, add_todo, add_book, delete_todo, mark_todo, unmark_todo
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -29,7 +29,9 @@ urlpatterns = [
     path("books/", books, name="books"),
     path("add_todo/", add_todo, name="add-todo"),
     path("add_book/", add_book, name="add-book"),
-    path("delete-todo/<id>/", delete_todo, name="delete-todo")
+    path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
+    path("unmark-todo/<id>/", unmark_todo, name="unmark-todo")
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
